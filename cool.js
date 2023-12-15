697d3e8c786ab1bae65f09cc935e6ec219d6fb34
Shrek4L1fE100 <jinuc.jung@stpaulhanoi.com>
2023-12-15 09:36:54 +0700

    Change

--- cool.js
+++ cool.js
@@ -1,24 +1,18 @@
-const titles = [
-    'important message',
-    'WIN FREE IPAD AND 5 MILLION ROBUX!',
-    'You won your 500,000$, CLICK HERE TO CLAIM IT!'
-  ]
-  
-  function changeTitles(titles){
-    // save an iterator in a closure
-    let ii = 0
-    // update is run at the start
-    return (function update() {
-      // change the title
-      document.querySelector('title').textContent = titles[(ii++ % titles.length)]
-      // queue the function to be called in 5 seconds
-      setTimeout(update, 5000)
-    })()
-  }
-  
-  changeTitles(titles)
+script.js
 
+let playBtn = document.getElementsByClassName("play");
+let pauseBtn = document.getElementsByClassName("pause");
 
-  <audio id="sound1" src="clickb6-101soundboards.mp3" preload="auto"></audio>
-  <button onclick="document.getElementById('sound1').play();">Play
-  it</button>
+let text = document.querySelector("p");
+
+const audio = new Audio("./clickb6-101soundboards.mp3");
+
+playBtn[0].addEventListener("click", (e) => {
+  audio.play();
+  text.innerHTML = "Audio Playing🔊";
+});
+
+pauseBtn[0].addEventListener("click", (e) => {
+  audio.pause();
+  text.innerHTML = "Audio Paused";
+});
